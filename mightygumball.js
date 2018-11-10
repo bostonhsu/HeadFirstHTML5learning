3fd8936e74a39065080cb013d81798d174b014ee
徐智勇 <bostonhsu@outlook.com>
2018-11-10 14:10:53 +0800

    Test JSONP.

--- mightygumball.js
+++ mightygumball.js
@@ -7,7 +7,7 @@
 window.onload = init;
 
 function init() {
-	getSales();
+	
 }
 
 //
@@ -18,7 +18,7 @@ function init() {
 function getSales_XHRv1() {
 	// change the URL to match the location where you
 	// put the sales.json file
-	var url = "http://localhost/gumball/sales.json";
+	var url = "http://gumball.wickedlysmart.com";
 	var request = new XMLHttpRequest();
 	request.open("GET", url);
 	request.onreadystatechange = function() {
@@ -37,7 +37,7 @@ function getSales_XHRv1() {
 function getSales() {
 	// change the URL to match the location where you
 	// put the sales.json file
-	var url = "http://localhost/gumball/sales.json";
+	var url = "http://gumball.wickedlysmart.com";
 	var request = new XMLHttpRequest();
 	request.open("GET", url);
 	request.onload = function() {
@@ -48,9 +48,8 @@ function getSales() {
 	request.send(null);
 }
 
-function updateSales(responseText) {
+function updateSales(sales) {
 	var salesDiv = document.getElementById("sales");
-	var sales = JSON.parse(responseText);
 	for (var i = 0; i < sales.length; i++) {
 		var sale = sales[i];
 		var div = document.createElement("div");
