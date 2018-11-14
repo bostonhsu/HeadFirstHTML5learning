2b1c755162cb38c24c503f7abe9b336429c9d9ce
徐智勇 <bostonhsu@outlook.com>
2018-11-14 10:38:45 +0800

    Test settimeval.

--- mightygumball.js
+++ mightygumball.js
@@ -3,11 +3,30 @@
  * get the content of a JSON file using Ajax 
  *
  */
+var lastReportTime = 0;
 
 window.onload = init;
 
 function init() {
-	
+	setInterval(handleRefresh, 3000);
+}
+
+function handleRefresh() {
+	var url = "http://gumball.wickedlysmart.com?callback=updateSales" + 
+	"&lastreporttime=" + lastReportTime +
+	"&random=" + (new Date()).getTime();
+
+	var newScriptElement = document.createElement("script");
+	newScriptElement.setAttribute("src", url);
+	newScriptElement.setAttribute("id", "jsonp");
+
+	var oldScriptElement = document.getElementById("jsonp");
+	var head = document.getElementsByTagName("head")[0];
+	if (oldScriptElement == null) {
+		head.appendChild(newScriptElement);
+	} else {
+		head.replaceChild(newScriptElement, oldScriptElement);
+	}
 }
 
 //
@@ -56,6 +75,9 @@ function updateSales(sales) {
 		div.setAttribute("class", "saleItem");
 		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
 		salesDiv.appendChild(div);
+	}
+	if (sales.length > 0) {
+		lastReportTime = sales[sales.length - 1].time;
 	}
 }
 
